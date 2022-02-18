53f60592d310d62099b5de94d84339a9c31d867f
Mohirbek <99872863+Mohirbek0797@users.noreply.github.com>
2022-02-18 00:02:57 -0800

    File updated

---
+++ forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Subject
+from .models import Teacher
+from django.forms import ModelForm
+
+
+class SpecialityForm(forms.Form):
+    name = forms.CharField(max_length=256)
+    code = forms.CharField(max_length=2048)
+    stat_data = forms.DateTimeField()
+    is_active = forms.BooleanField(required=True)
+
+
+class TeacherCreateForm(ModelForm):
+    class Meta:
+        model = Teacher
+        fields = ['first_name', 'last_name', 'degree', ]
+
+
+class SubjectCreateForm(ModelForm):
+    class Meta:
+        model = Subject
+        fields = ['name', 'Specialities', 'teachers', ]
---
+++ models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+from django.urls import reverse
+
+
+class Speciality(models.Model):
+    name = models.CharField(max_length=256)
+    code = models.CharField(max_length=2048)
+    stat_data = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'Speciality'
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    degree = models.CharField(max_length=256)
+
+    class Meta:
+        db_table = 'Teacher'
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name}'
+
+    @staticmethod
+    def get_absolute_url():
+        return reverse('courses:index')
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=256)
+    Specialities = models.ManyToManyField(Speciality)
+    teachers = models.ManyToManyField(Teacher)
+
+    class Meta:
+        db_table = 'Subject'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
